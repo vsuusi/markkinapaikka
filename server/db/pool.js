8954b2f0b5baed489ae4d79d534bc594f912e498
Santeri Uusitalo <vs.uusitalo@gmail.com>
2024-05-05 18:05:05 +0300

    updated database connection

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'markkinapaikka_password',
-  database: 'markkinapaikka_db',
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 export default pool;
